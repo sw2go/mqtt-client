06beb7c8614d78db5738445a52bbfcf6f353e8b4
aa <aac1291@gmx.ch>
2022-12-13 18:13:58 +0100

    update

--- src/dom.ts
+++ src/dom.ts
@@ -47,7 +47,6 @@ export class Dom {
 
 }
 
-
 export class Div {
     private div: HTMLDivElement = null;
     constructor(id: string) {
@@ -72,7 +71,112 @@ export class Div {
     public get Element(): HTMLDivElement {
         return this.div;
     }
-
-
 }
+
+export class UI {
+    constructor () { }
+    private lwData = new UIMsg("lw");
+    private pubData = new UIMsg("pub");
+    private subData = new UISub("sub");
+    public get Host() { return Dom.inp("host").value; }
+    public get User() { return Dom.inp("user").value; }
+    public get Password() { return Dom.inp("password").value; }
+    public get LastWill() { return this.lwData; }
+    public get Reconnect() { return Dom.inp("reconnect").checked; }
+    public get CleanSession() { return Dom.inp("clean-session").checked; }
+    public get ClientId() { return Dom.inp("client-id").value.trim(); }
+    public get Pub() { return this.pubData; }
+    public get Sub() { return this.subData; }
+}
+
+class UISub {
+    constructor (protected prefix: string) { }
+    public get Topic() { return Dom.inp(`${this.prefix}-topic`).value.trim(); }
+    public get Qos() { return parseInt(Dom.inp(`${this.prefix}-qos`).value); }
+}
+  
+class UIMsg extends UISub {
+    constructor ( prefix: string) { super(prefix); }
+    public get Message() { return Dom.inp(`${this.prefix}-message`).value.trim(); }
+    public get Retained() { return Dom.inp(`${this.prefix}-retained`).checked; }
+}
+
+
+export class Tab {    
+    public static activate(button: EventTarget, tabContentDivId) {        
+        // Hide all "tabcontent" 
+        let tabcontent = document.getElementsByClassName("tabcontent") as HTMLCollectionOf<HTMLDivElement>;
+        for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        }
+
+        // Remove the class "active" from "tablinks"
+        let tablinks = document.getElementsByClassName("tablinks");
+        for (let i = 0; i < tablinks.length; i++) {
+            tablinks[i].classList.remove("active");
+        }
+
+        // Show the current "tabcontent" and set "tablink" "active"
+        Dom.div(tabContentDivId).style.display = "block";
+        (button as HTMLElement).classList.add("active");
+    }
+}
+
+
+export class Chip {
+    public static create(topic: string, clientId: string, cleanSession: boolean, qos: number, unsubscribe: (HTMLElement) => void): HTMLDivElement {        
+        if (Dom.div("subs").querySelector(`[topic="${topic}"][client-id="${clientId}"]`)) {
+            return null; // chip/subscription already exists
+        } else {
+            let chip = document.createElement('div') as HTMLDivElement;
+            chip.classList.add("chip");
+            chip.setAttribute("clean-session", cleanSession ? "1" : "0");
+            chip.setAttribute("client-id", clientId);
+            chip.setAttribute("topic", topic);
+            chip.setAttribute("qos", qos.toString());
+            chip.innerText = topic;
+            
+            let close = document.createElement('span') as HTMLSpanElement;
+            close.classList.add("chip-x");
+            close.innerHTML = "&times;";
+            close.addEventListener("click", () => unsubscribe(chip));  
+            chip.appendChild(close);
+            
+            let tooltip = document.createElement('span') as HTMLSpanElement;
+            tooltip.classList.add("tooltiptext");
+            tooltip.innerText = cleanSession ? `clean(${clientId}) qos=${qos}` :  `${clientId} qos=${qos}`
+            chip.appendChild(tooltip);
+
+            return chip;
+        }
+      }
+
+      public static add(chip: HTMLDivElement) {
+        Dom.div("subs").appendChild(chip);  
+      }
+
+      public static remove(chip: HTMLDivElement) {
+        chip.remove();
+      }
+
+      public static allCleanSession() {
+        // any chip with clean-session
+        return Dom.div("subs").querySelectorAll(`.chip[clean-session="1"]`) as NodeListOf<HTMLDivElement>;
+      }
+
+      public static isCleanSession(chip: HTMLDivElement) {
+        return chip.getAttribute("clean-session") === "1";
+      }
+
+      public static isSameClient(chip: HTMLDivElement, clientId: string) {
+        return chip.getAttribute("client-id") === clientId;
+      }
+
+      public static Topic(chip: HTMLDivElement) {
+        return chip.getAttribute("topic");
+      }
+}
+
+
+
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./css/main.css";
 import * as Paho from "paho-mqtt"
-import { Dom, Div } from "./dom";
+import { Dom, Div, UI, Tab, Chip } from "./dom";
 
 enum State {
   Disconnected = 0,
@@ -24,18 +24,27 @@ class Main {
   private msg: Div = new Div("messages");
   private log: Div = new Div("loggings");
 
+  private ui = new UI();
+
   constructor() {
     this.client = null;
     this.updateStatus(State.Disconnected);
     
     Dom.btn("connect").addEventListener("click", () => this.connection() );
     Dom.btn("disconnect").addEventListener("click", () => this.connection() );
+    Dom.btn("lastwill").addEventListener("click", (e) => this.toogleAccordion(e.target as HTMLButtonElement) );
 
     Dom.btn("pub").addEventListener("click", () => this.publish());
     Dom.btn("sub").addEventListener("click", () => this.subscribe());
-    Dom.btn("messages").addEventListener("click", (e) => this.activateTab(e, "messages"));
-    Dom.btn("loggings").addEventListener("click", (e) => this.activateTab(e, "loggings"));
+    Dom.btn("messages").addEventListener("click", (e) => Tab.activate(e.currentTarget, "messages"));
+    Dom.btn("loggings").addEventListener("click", (e) => Tab.activate(e.currentTarget, "loggings"));
     Dom.btn("messages").click();
+    Dom.btn("clear").addEventListener("click", () => this.clear());
+  }
+
+  private toogleAccordion(btn: HTMLButtonElement) {
+    btn.classList.toggle("active");
+    btn.nextElementSibling.classList.toggle("hide");  // toggle visibility of Element after the button
   }
 
   private connection() {
@@ -48,37 +57,18 @@ class Main {
     }
   }
 
-
-
-  private activateTab(e, name) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    Dom.div(name).style.display = "block";
-    e.currentTarget.className += " active";
+  private clear() {
+    this.msg.Element.innerHTML ="";
+    this.log.Element.innerHTML ="";
   }
-
 
   private connect() {
     this.updateStatus(State.Connecting);
 
-    let clientId: string = Dom.inp("client-id").value;
+    let clientId: string = this.ui.ClientId;
 
-    if (!clientId || clientId.trim().length == 0) {
-      if (Dom.inp("clean-session").checked) {
+    if (this.ui.ClientId.length == 0) {
+      if (this.ui.CleanSession) {
         clientId = "randomId_" + Math.random().toString(16).substring(2,8);
       } else {
         alert(`Client Id is mandatory when using session`);
@@ -86,28 +76,40 @@ class Main {
       }
     }
 
-    this.client = new Paho.Client(Dom.inp("host").value, 8884, clientId);
+    this.client = new Paho.Client(this.ui.Host, 8884, clientId);
     this.client.onMessageDelivered = m => this.messageDelivered(m);
     this.client.onMessageArrived = m => this.messageArrived(m);
     this.client.onConnectionLost = e => this.connectionLost(e);
 
-    try {
-      this.client.connect({
-        useSSL: true,
-        reconnect: Dom.inp("reconnect").checked,
-        cleanSession: Dom.inp("clean-session").checked,
-        userName: Dom.inp("user").value,
-        password: Dom.inp("password").value,
-        onSuccess: (o: Paho.WithInvocationContext) => {
-          this.updateStatus(State.Connected, clientId);
-        },
-        onFailure: (e: Paho.ErrorWithInvocationContext) => {
-          this.updateStatus(State.ConnectFailure, `${clientId} error: ${e.errorMessage}`);
-        }
-      });
-    } catch (e) {
-      this.updateStatus(State.ConnectInvalidState, `${clientId}`);
+    let options: Paho.ConnectionOptions = {
+      useSSL: true,
+      reconnect: this.ui.Reconnect,
+      cleanSession: this.ui.CleanSession,
+      userName: this.ui.User,
+      password: this.ui.Password,        
+      onSuccess: (o: Paho.WithInvocationContext) => {
+        this.updateStatus(State.Connected, clientId);
+      },
+      onFailure: (e: Paho.ErrorWithInvocationContext) => {
+        this.updateStatus(State.ConnectFailure, `${clientId} error: ${e.errorMessage}`);
+      } 
     }
+
+    if (this.ui.LastWill.Message.length > 0 && this.ui.LastWill.Topic.length) {
+      let lw = new Paho.Message(this.ui.LastWill.Message);
+      lw.destinationName = this.ui.LastWill.Topic;
+      lw.qos = this.ui.LastWill.Qos as Paho.Qos;
+      lw.retained = this.ui.LastWill.Retained;
+      console.log(lw);
+      options.willMessage = lw;
+    }
+
+    try {
+      this.client.connect(options);
+    } catch (e: any) {
+      this.updateStatus(State.ConnectInvalidState, `${clientId} error: ${e.message}`);
+    }
+
   }
 
   private disconnect() {
@@ -117,14 +119,12 @@ class Main {
       this.client.disconnect();
     } catch (e) {
       this.updateStatus(State.DisconnectInvalidState);
-    }    
-    this.msg.Element.innerHTML ="";
-    this.log.Element.innerHTML ="";
+    }
   }
 
   private reset() {
     if (this.client) {
-      if (this.client.isConnected) {
+      if (this.client.isConnected()) {
         this.unsubscribeAll();
         try {
           this.client.disconnect();
@@ -138,31 +138,29 @@ class Main {
   }
 
   private publish() {      
-    let qos = parseInt(Dom.inp("pub-qos").value) as Paho.Qos;
     try {
-      this.client.send(Dom.inp("pub").value, Dom.inp("message").value, qos, Dom.inp("retained").checked); 
+      this.client.send( 
+        this.ui.Pub.Topic, 
+        this.ui.Pub.Message, 
+        this.ui.Pub.Qos as Paho.Qos, 
+        this.ui.Pub.Retained); 
     } catch (e) {
       this.updateStatus(State.PublishInvalidState);
     } 
   }
 
   private subscribe() {
-    let clientId = Dom.inp("client-id").value;
-    let topic = Dom.inp("sub").value;
-    let qos = parseInt(Dom.inp("sub-qos").value) as Paho.Qos;
-
-    // create chip only if it does not jet exist for the current session 
-    if (!Dom.div("subs").querySelector(`[topic="${topic}"][client-id="${clientId}"]`)) {
-      let newDiv = this.createChip(topic);      
+    let chip = Chip.create(this.ui.Sub.Topic, this.ui.ClientId, this.ui.CleanSession, this.ui.Sub.Qos, (e) => this.unsubscribe(e));  
+    if (chip) {
       try {
-        this.client.subscribe(topic, { 
-          qos: qos,
+        this.client.subscribe(this.ui.Sub.Topic, { 
+          qos: this.ui.Sub.Qos as Paho.Qos,
           onSuccess: (o => {
-            Dom.div("subs").appendChild(newDiv);  
-            this.log.Text(`subscribe: ${topic} success, grantedQos: ${o.grantedQos}`);
+            Chip.add(chip);
+            this.log.Text(`subscribe: ${this.ui.Sub.Topic} success, grantedQos: ${o.grantedQos}`);
           }),
           onFailure: (e => {
-            this.log.Text(`subscribe: ${topic} failure, ${e.errorMessage}`);
+            this.log.Text(`subscribe: ${this.ui.Sub.Topic} failure, ${e.errorMessage}`);
           })        
         });  
       }catch (e) {
@@ -171,52 +169,35 @@ class Main {
     }
   }
 
-  private createChip(topic: string): HTMLDivElement {
-    let chip = document.createElement('div') as HTMLDivElement;
-    chip.classList.add("chip");
-    chip.setAttribute("clean-session", Dom.inp("clean-session").checked ? "1" : "0");
-    chip.setAttribute("client-id", Dom.inp("client-id").value);
-    chip.setAttribute("topic", topic);
-    chip.setAttribute("qos", Dom.inp("sub-qos").value);
-    chip.innerText = topic;
-    let newSpan = document.createElement('span') as HTMLSpanElement;
-    newSpan.classList.add("chip-x");
-    newSpan.innerHTML = "&times;";
-    chip.appendChild(newSpan);
-    newSpan.addEventListener("click", () => { 
-      if (chip.getAttribute("clean-session") === "1" || chip.getAttribute("client-id") === Dom.inp("client-id").value) {
-        this.unsubscribe(chip);
-      } else {
-        alert("to unsubscribe use correct client-id: " + chip.getAttribute("client-id"));
-      }
-    });  
-    return chip;
-  }
-
   private unsubscribeAll() {
-    let chip = Dom.div("subs").lastElementChild as HTMLDivElement;
-    // unsubscribe all only when connection was a clean-session 
-    if (chip && chip.classList.contains("chip") && chip.getAttribute("clean-session") === "1") {      
-      this.unsubscribe(chip);
-      this.unsubscribeAll();
-    }  
+    let chips = Chip.allCleanSession();
+    for (let i = 0; i < chips.length; i++) {
+      this.unsubscribe(chips[i]);
+    }
   }
 
-  private unsubscribe(div: HTMLElement) {
-    let topic = div.getAttribute("topic");
-    try {
-      this.client.unsubscribe(topic, {
-        onSuccess: (o => {
-          this.log.Text(`unsubscribe: ${topic} success`);
-        }),
-        onFailure: (e => {
-          this.log.Text(`unsubscribe: ${topic} failure, ${e.errorMessage}`);
-        }) 
-      });
-    } catch(e) {   
-      this.updateStatus(State.UnsubscribeInvalidState)   
+  private unsubscribe(chip: HTMLDivElement) {
+    if (this.client?.isConnected()) {
+      if (Chip.isCleanSession(chip) || Chip.isSameClient(chip, this.ui.ClientId)) {
+        try {
+          this.client.unsubscribe(Chip.Topic(chip), {
+            onSuccess: (o => {
+              this.log.Text(`unsubscribe: ${Chip.Topic(chip)} success`);
+              Chip.remove(chip);
+            }),
+            onFailure: (e => {
+              this.log.Text(`unsubscribe: ${Chip.Topic(chip)} failure, ${e.errorMessage}`);
+            }) 
+          });
+        } catch(e) {   
+          this.updateStatus(State.UnsubscribeInvalidState)   
+        }  
+      }
+    } else if (Chip.isCleanSession(chip)) {
+      Chip.remove(chip);  // not connected with clean session -> subscription is lost anyway
+    } else {
+      // keep chip, it belongs to another session or the client is not connected
     }
-    div.remove();
   }
 
   private messageDelivered(message: Paho.Message) {
